80584dde0b76f0f9aa2843eeaff88afb01226c41
xmrtl777 <shevyakov1423d@gmail.com>
2025-12-05 04:46:04 +0300

    v with changed index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+if ((window as any).Telegram?.WebApp) {
+  (window as any).Telegram.WebApp.ready()
+  (window as any).Telegram.WebApp.expand()
+}
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(
   <React.StrictMode>
